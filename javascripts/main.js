5ebdcaa4513d6fbd6f937f39bc08a581a4a7c711
廖倚韓 <yihanliao@gamasys.com.tw>
2020-05-27 15:15:24 +0800

    測試安裝功能

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,16 +6,55 @@ if (window.matchMedia('(display-mode: standalone)').matches) {
 } else {
   pwadetectDOM.innerText = 'This is running as normal mode.';
   pwadetectDOM.style.color = 'lightblue';
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    document.getElementById('promotionInfo').display = 'block';
+  });
 }
+
+document.getElementById('promotionInfo').addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  document.getElementById('promotionInfo').display = 'none';
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
 
 const showiframe = function() {
   document.getElementById('lunchGameWrapper').style.display = 'block';
   // document.getElementById('webframe').src = '';
   document.getElementById('webframe').src = 'https://game.jdb168.net/?lang=th&isHiddenCloseIcon=1&origin=&isAPP=false&d=1&gameType=8&mType=8048&gName=OpenSesameII_f1b503e&e=jdb168&more=1&mute=0&x=e9tkQRED2CABTGFfK6IMPqhtbzMrTJRk8_PyR81NWNAQWKlEmQh8WCIK-1CelNAHfYNZ-HA8ZVjQsUuDUfvS3KUkABYLxia4UnD6H3pRCAACDcrkt21D6lLswmXvH-iDXPD4Cty6hS0';
-  document.getElementById('webframe').height = window.innerHeight;
+  document.getElementById('webframe').height = '200px';
 }
 
 const hideiframe = function() {
   document.getElementById('lunchGameWrapper').style.display = 'none';
   document.getElementById('webframe').src = '';
 }
+
+window.addEventListener("orientationchange", function(e) {
+  // let targetHeight = (window.innerWidth - window.innerHeight > 0) ? "window.innerWidth" : "window.innerHeight";
+  // document.getElementById('orientationdetect').innerHTML = window.orientation + ':' + window.innerHeight + '****' + targetHeight + '****';
+  // document.getElementById('orientationdetect').innerHTML = screen.availHeight;
+  let targetHeight;
+  switch (window.orientation) {
+    case 90:
+    case -90:
+      targetHeight = (window.innerWidth - window.innerHeight > 0) ? window.innerHeight : window.innerWidth;
+      break;
+    default:
+      targetHeight = (window.innerWidth - window.innerHeight > 0) ? window.innerWidth : window.innerHeight;
+  }
+  document.getElementById('orientationdetect').innerHTML = window.orientation + ':' + window.innerHeight + '****' + targetHeight + '****';
+});
