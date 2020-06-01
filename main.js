4323bee0a2c823837dae5f5de95ae183826b5537
廖倚韓 <yihanliao@gamasys.com.tw>
2020-06-01 17:43:19 +0800

    偵測安裝功能

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
 let pwadetectDOM = document.getElementById('pwadetect');
-document.getElementById('jstext').innerHTML = '我要測試彈窗功能 v1-1';
+document.getElementById('jstext').innerHTML = '我要測試PWA 安裝偵測 v1';
+// var vConsole = new VConsole();
 let deferredPrompt;
 if (window.matchMedia('(display-mode: standalone)').matches) {
   console.log("This is running as standalone.");
   pwadetectDOM.innerText = 'This is running as standalone.';
   pwadetectDOM.style.color = 'lightgreen';
+  // var vConsole = new VConsole();
 } else {
   pwadetectDOM.innerText = 'This is running as normal mode.';
   pwadetectDOM.style.color = 'lightblue';
+}
+
+window.addEventListener('popstate', (event) => {
+  quityes();
+});
+
+const xhrGenerator = ({ url, data }) => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    xhttp.send(data);
+    xhttp.onreadystatechange = function fianlly() {
+      let responseText;
+      try {
+        responseText = (this.readyState == 4 && this.response) ? JSON.parse(this.response) : '';
+      } catch (e) {
+        console.error(e)
+      }
+      if (this.readyState == 4 && this.status == 200) {
+        resolve(responseText);
+      }
+      if (this.readyState == 4 && this.status !== 200) {
+        reject(responseText);
+      }
+    }
+  })
+}
+
+const getInnerHeight = () => {
+  let targetHeight;
+  switch (window.orientation) {
+    case 90:
+    case -90:
+      targetHeight = (window.innerWidth - window.innerHeight > 0) ? window.innerHeight : window.innerWidth;
+      break;
+    default:
+      targetHeight = (window.innerWidth - window.innerHeight > 0) ? window.innerWidth : window.innerHeight;
+  }
+  return targetHeight;
 }
 
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -35,30 +77,60 @@ document.getElementById('promotionInfo').addEventListener('click', (e) => {
   });
 });
 
+window.addEventListener('appinstalled', () => {
+  console.log('PWA Installed.')
+});
+
 const showiframe = function() {
-  document.getElementById('lunchGameWrapper').style.display = 'block';
-  // document.getElementById('webframe').src = '';
-  document.getElementById('webframe').src = 'https://game.jdb168.net/?lang=th&isHiddenCloseIcon=1&origin=&isAPP=false&d=1&gameType=8&mType=8048&gName=OpenSesameII_f1b503e&e=jdb168&more=1&mute=0&x=e9tkQRED2CABTGFfK6IMPqhtbzMrTJRk8_PyR81NWNAQWKlEmQh8WCIK-1CelNAHfYNZ-HA8ZVjQsUuDUfvS3KUkABYLxia4UnD6H3pRCAACDcrkt21D6lLswmXvH-iDXPD4Cty6hS0';
-  document.getElementById('webframe').height = '200px';
+  return xhrGenerator({
+    url: '/api/game/launchUrl/demo',
+    data: JSON.stringify({
+      deviceType: 0,
+      domain: document.location.origin,
+      gameBranch: "JDB",
+      gameId: 4790,
+      gameLang: "th",
+    })
+  })
+  .then(({ data }) => {
+    document.getElementById('lunchGameWrapper').style.display = 'block';
+    document.getElementById('lunchGameWrapper').style.height = `${getInnerHeight()}px`;
+    document.getElementById('webframe').src = data.gameLaunch;
+    document.getElementById('webframe').height = getInnerHeight();
+  })
+  .catch((e) => {
+    console.log(e);
+  })
 }
 
-const hideiframe = function() {
+const showiframeWithFull = function() {
+  showiframe()
+  .then(() => {
+    console.log('fullscreen??')
+    document.getElementById('webframe').requestFullscreen()
+  });
+}
+
+const backBtnTrigger = function() {
+  document.getElementById('webframeBackButton').style.display = 'none';
+  document.getElementById('confirmDialog').style.display = 'block';
+}
+
+const quityes = function() {
   document.getElementById('lunchGameWrapper').style.display = 'none';
+  document.getElementById('confirmDialog').style.display = 'none';
+  document.getElementById('webframeBackButton').style.display = 'block';
   document.getElementById('webframe').src = '';
 }
 
+const quitno = function() {
+  document.getElementById('webframeBackButton').style.display = 'block';
+  document.getElementById('confirmDialog').style.display = 'none';
+}
+
 window.addEventListener("orientationchange", function(e) {
-  // let targetHeight = (window.innerWidth - window.innerHeight > 0) ? "window.innerWidth" : "window.innerHeight";
-  // document.getElementById('orientationdetect').innerHTML = window.orientation + ':' + window.innerHeight + '****' + targetHeight + '****';
-  // document.getElementById('orientationdetect').innerHTML = screen.availHeight;
-  let targetHeight;
-  switch (window.orientation) {
-    case 90:
-    case -90:
-      targetHeight = (window.innerWidth - window.innerHeight > 0) ? window.innerHeight : window.innerWidth;
-      break;
-    default:
-      targetHeight = (window.innerWidth - window.innerHeight > 0) ? window.innerWidth : window.innerHeight;
-  }
-  document.getElementById('orientationdetect').innerHTML = window.orientation + ':' + window.innerHeight + '****' + targetHeight + '****';
+  document.getElementById('webframe').height = getInnerHeight();
+  document.getElementById('lunchGameWrapper').style.height = `${getInnerHeight()}px`;
+  document.getElementById('orientationdetect').innerHTML = window.orientation;
 });
+
